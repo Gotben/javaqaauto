fda17ec17367304d6c178f23e723ab3d21a78e2b
Gotben <gotbanzot@gmail.com>
2024-07-17 19:06:53 +0300

    добавлены 2 новые функции

--- sandbox/src/main/java/ru/qa/Programmtest.java
+++ sandbox/src/main/java/ru/qa/Programmtest.java
@@ -3,7 +3,25 @@ package ru.qa;
 public class Programmtest {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        hello("world");
+        double l = 2;
+        System.out.println(area(l));
+        double a = 5;
+        double b = 2;
+        System.out.println(square(a,b));
+    }
+
+    public static void hello(String s) {
+        System.out.println("Hello, " + s + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+
+    }
+
+    public static double square(double a, double b) {
+        return a * b;
     }
 
 }
